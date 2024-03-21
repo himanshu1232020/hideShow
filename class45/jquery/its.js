4d704f67464a61cccfcd5d9c965290570d20d552
himanshu1232020 <164318065+himanshu1232020@users.noreply.github.com>
2024-03-21 13:18:18 +0530

    Add files via upload

---
+++ class45/jquery/its.js
@@ -0,0 +1,23 @@
+$(function (){
+$('#inp1').click(function(){
+    $('p').hide()
+});
+$('#inp2').click(function(){
+$('p').show()
+});
+$('#inp3').click(function(){
+    $('p').toggle()
+    });
+    });
+
+
+
+
+
+
+
+ 
+
+
+
+
